285af81a25bd66e52b901e79e0562316974a58b2
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-19 20:52:38 +0000

    Added a BMI Interpreter to notify users where they stand on the BMI spectrum. Also moved formulas too their own methods.

--- Exercise2/Solution.java
+++ Exercise2/Solution.java
@@ -8,23 +8,55 @@ public class Solution {
     static double height;
     static float BMI; 
 
-    public static void main(String[] args)  {
-
-        System.out.println("Please enter the following details to recieve your Body Mass Index.");
-        System.out.print("What is your Name?: ");
-        name = kb.next();
-        System.out.print("What is your Weight in kg?: ");
-        weight = kb.nextInt();
-        System.out.print("What is your height in cm's?: ");
-        height = kb.nextInt();
-        BMI = (float)(weight/(height*height))*10000;
-
-        System.out.printf(name+ " your current Body Mass Index Reading is : "+"%.2f",BMI);
-
-
+        public static void main(String[] args)  {
+    
+            System.out.println("Please enter the following details to recieve your Body Mass Index.");
+            System.out.print("What is your Name?: ");
+            name = kb.next();
+            System.out.print("What is your Weight in kg?: ");
+            weight = kb.nextInt();
+            System.out.print("What is your height in cm's?: ");
+            height = kb.nextInt();
+            BMI = bmiCalculator(weight,height);
+            System.out.printf(name+ " your current Body Mass Index Reading is : "+"%.2f  \n",BMI);
+            System.out.println("These results mean that you are "+bmiInterpreter(BMI)+".");
 
 
 
+
+
+    }
+    
+        static float bmiCalculator(double w, double h){
+            
+            float BMI;
+            
+            BMI=(float)(w/(h*h))*10000;
+            
+            return BMI;
+            
+        }
+    
+        static String bmiInterpreter(float bmi) {
+        
+                String interpret = "";
+            
+                if (bmi < 18.5 ) {
+                    interpret= "Underweight";
+                }
+        
+                else if (bmi >= 18.5 && bmi < 25) {
+                   interpret= "Normal Weight";
+                }
+        
+                else if (bmi >= 25 && bmi < 30) {
+                    interpret= "Overweight";
+                }
+        
+                else if (bmi >= 30) {
+                   interpret= "Obese";
+                }
+            return interpret;
     }
 
     }
