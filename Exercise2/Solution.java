3bd3e189105fa503bdae28e3175d8eaf690b9114
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-19 19:15:25 +0000

    Added data gathering functionality for program via terminal inputs

---
+++ Exercise2/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static Scanner kb = new Scanner(System.in);
+    static String name;
+    static char sex;
+    static int weight;
+    static int age;
+    static int age;
+
+    public static void main(String[] args)  {
+
+        System.out.print("What is your Name?: ");
+        name = kb.next();
+        System.out.print("How many years have you been on this planet?: ");
+        age = kb.nextInt();
+        System.out.print("Ar you Male (M) or Female (F) : ");
+        sex = kb.next();
+        System.out.print("What is your Weight in kg?: ");
+        weight = kb.next();
+        System.out.print("What is your height in cm's?: ");
+        height = kb.next();
+        
+
+
+        System.out.print("Your name is "+name+", you are "+age+" years old, and your username is " +userName);
+
+
+    }
+
+    }
+
+
+
