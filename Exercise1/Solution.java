577ce7c10a1752c26a964b21875be793b1e33dcc
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-19 18:09:24 +0000

    Added basic functionality for the user to povide inputs and recieve outputs via the Terminal

---
+++ Exercise1/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    static Scanner kb = new Scanner(System.in);
+    static String name;
+    static String userName;
+    static int age;
+
+    public static void main(String[] args)  {
+
+        System.out.print("What is your Name?: ");
+        name = kb.next();
+        System.out.print("How many years have you been on this planet?: ");
+        age = kb.nextInt();
+        System.out.print("What is your User Name?: ");
+        userName = kb.next();
+
+
+        System.out.print("Your name is "+name+", you are "+age+" years old, and your username is " +userName);
+
+
+    }
+
+    }
+
+
+
