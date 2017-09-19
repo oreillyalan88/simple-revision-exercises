89e3984d844c358ac3cfed77fde59d3618c1ae15
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-19 20:08:33 +0000

    Removed redundnat variables for now. Added BMI formula, ability to calculate BMI results and print them to the user

--- Exercise2/Solution.java
+++ Exercise2/Solution.java
@@ -4,27 +4,25 @@ public class Solution {
 
     static Scanner kb = new Scanner(System.in);
     static String name;
-    static char sex;
-    static int weight;
-    static int age;
-    static int age;
+    static double weight;
+    static double height;
+    static float BMI; 
 
     public static void main(String[] args)  {
 
+        System.out.println("Please enter the following details to recieve your Body Mass Index.");
         System.out.print("What is your Name?: ");
         name = kb.next();
-        System.out.print("How many years have you been on this planet?: ");
-        age = kb.nextInt();
-        System.out.print("Ar you Male (M) or Female (F) : ");
-        sex = kb.next();
         System.out.print("What is your Weight in kg?: ");
-        weight = kb.next();
+        weight = kb.nextInt();
         System.out.print("What is your height in cm's?: ");
-        height = kb.next();
-        
+        height = kb.nextInt();
+        BMI = (float)(weight/(height*height))*10000;
+
+        System.out.printf(name+ " your current Body Mass Index Reading is : "+"%.2f",BMI);
 
 
-        System.out.print("Your name is "+name+", you are "+age+" years old, and your username is " +userName);
+
 
 
     }
